9ea24ba39fe390a4245103e13875d9f5b0e8810b
Tom <tom.martens01@gmail.com>
2024-10-28 17:32:59 +0100

    server testing finnaly hopefully

--- Server/Program.cs
+++ Server/Program.cs
@@ -14,27 +14,27 @@ public class Program
     private const string SAVE_FILE_NAME = "TaskInJsonFormatCBD.json";
     //private static int taskIdCounter = 1;
 
-    private static TcpListener server;
-    private static List<TcpClient> clients = new List<TcpClient>();
-    private static List<TaskItem> tasksItems = new List<TaskItem>();
+    public static TcpListener TCPServer { get; private set; }
+    public static List<TcpClient> Clients { get; private set; } = new List<TcpClient>();
+    public static List<TaskItem> TasksItems { get; private set; } = new List<TaskItem>();
     
     public static void Main(string[] args)
     {
-        loadFromFile();
+        loadFromFile(SAVE_FILE_NAME);
 
-        server = new TcpListener(IPAddress.Any, 1234);
-        server.Start();
+        TCPServer = new TcpListener(IPAddress.Any, 1234);
+        TCPServer.Start();
         Console.WriteLine("server started");
 
         while (true)
         {
             try
             {
-                var tcpClient = server.AcceptTcpClient();
+                var tcpClient = TCPServer.AcceptTcpClient();
                 Console.WriteLine($"client connected = {tcpClient.Client.RemoteEndPoint}");
-                clients.Add(tcpClient);
+                Clients.Add(tcpClient);
                 new Thread(() => { ListenForMessages(tcpClient); }).Start();
-                Console.WriteLine($"connections open: {clients.Count}");
+                Console.WriteLine($"connections open: {Clients.Count}");
             } catch (Exception exp)
             {
                 Console.WriteLine(exp.ToString());
@@ -93,17 +93,17 @@ public class Program
 
         //exited whileloop: connection closed
         Console.WriteLine("connection closed");
-        clients.Remove(tcpClient);
-        Console.WriteLine($"connections open: {clients.Count}");
+        Clients.Remove(tcpClient);
+        Console.WriteLine($"connections open: {Clients.Count}");
     }
     public static int GenerateUniqueId() 
     {
-        if (tasksItems.Count == 0)
+        if (TasksItems.Count == 0)
         {
             return 1;
         }
 
-        var existingIds = tasksItems.Select(task => int.Parse(task.Id)).ToList();
+        var existingIds = TasksItems.Select(task => int.Parse(task.Id)).ToList();
         existingIds.Sort();
 
         for (int i = 1; i <= existingIds.Count; i++)
@@ -116,36 +116,36 @@ public class Program
 
         return existingIds.Count + 1;
     }
-    private static void AddTask(TaskItem task)
+    public static void AddTask(TaskItem task)
     {
         task.Id = GenerateUniqueId().ToString();
-        tasksItems.Add(task);
+        TasksItems.Add(task);
         Console.WriteLine($"task added = id: {task.Id}, name: {task.Name}, description: {task.Description}, state = {task.State.ToString()}");
         BroadcastUpdate();
-        saveToFile();
+        saveTasksToFile(SAVE_FILE_NAME);
         //saveCounterToFile();
     }
 
-    private static void RemoveTask(TaskItem task)
+    public static void RemoveTask(TaskItem task)
     {
         string taskId = task.Id;
-        tasksItems.RemoveAll(tasksItem => tasksItem.Id == taskId);
+        TasksItems.RemoveAll(tasksItem => tasksItem.Id == taskId);
         Console.WriteLine($"removed task = id: {task.Id}, name: {task.Name}, description: {task.Description}, state = {task.State.ToString()}");
         BroadcastUpdate();
-        saveToFile();
+        saveTasksToFile(SAVE_FILE_NAME);
     }
 
-    private static void EditTask(TaskItem task)
+    public static void EditTask(TaskItem task)
     {
-        TaskItem? existingTaskItem = tasksItems.Where(taskItem => taskItem.Id == task.Id).FirstOrDefault();
-        int index = tasksItems.IndexOf(existingTaskItem);
+        TaskItem? existingTaskItem = TasksItems.Where(taskItem => taskItem.Id == task.Id).FirstOrDefault();
+        int index = TasksItems.IndexOf(existingTaskItem);
         if (index != -1)
         {
-            tasksItems[index] = task;
+            TasksItems[index] = task;
             Console.WriteLine($"edited task = id: {existingTaskItem.Id}, name: {existingTaskItem.Name}, description: {existingTaskItem.Description}");
             Console.WriteLine($"into task = id: {task.Id}, name: {task.Name}, description: {task.Description}, state = {task.State.ToString()}");
             BroadcastUpdate();
-            saveToFile();
+            saveTasksToFile(SAVE_FILE_NAME);
         } else
         {
             Console.WriteLine($"failed attempt to edit task = id: {task.Id}, name: {task.Name}, description: {task.Description}, state = {task.State.ToString()}");
@@ -153,10 +153,10 @@ public class Program
     }
 
 
-    private static void BroadcastUpdate()
+    public static void BroadcastUpdate()
     {
         Console.WriteLine("sending update to all clients =");
-        foreach (var client in clients)
+        foreach (var client in Clients)
         {
             SendClientUpdate(client);
         }
@@ -165,22 +165,22 @@ public class Program
     private static void SendClientUpdate(TcpClient tcpClient)
     {
         Console.WriteLine($"send update to client: {tcpClient.Client.RemoteEndPoint}");
-        var tasksArray = tasksItems.ToArray();
+        var tasksArray = TasksItems.ToArray();
         NetworkJsonObject networkJsonObject;
         networkJsonObject = new NetworkJsonObject() { Status = StatusType.Get, Items = tasksArray };
         ClientServerUtils.SendNetworkJsonObject(tcpClient.GetStream(), networkJsonObject);
     }
 
-    private static void saveToFile()
+    public static void saveTasksToFile(string file_name)
     {
-        
-        string jsonTasks = JsonConvert.SerializeObject(tasksItems, Formatting.Indented);
+        string jsonTasks = JsonConvert.SerializeObject(TasksItems, Formatting.Indented);
         string werkdirectory = Environment.CurrentDirectory;
-        string path = Path.Combine(werkdirectory, SAVE_FILE_NAME);
+        string path = Path.Combine(werkdirectory, file_name);
         File.WriteAllText(path, jsonTasks);
         
         Console.WriteLine($"tasks saved in file: {path}");
     }
+
     //private static void saveCounterToFile()
     //{
     //    File.WriteAllText("SAVED_COUNTER", taskIdCounter.ToString());
@@ -196,16 +196,16 @@ public class Program
     //}
 
 
-    private static void loadFromFile()
+    public static void loadFromFile(string file_name)
     {
-        if (File.Exists(SAVE_FILE_NAME))
+        if (File.Exists(file_name))
         {
             {
-                string json = File.ReadAllText(SAVE_FILE_NAME);
-                tasksItems = JsonConvert.DeserializeObject<List<TaskItem>>(json) ?? [];
+                string json = File.ReadAllText(file_name);
+                TasksItems = JsonConvert.DeserializeObject<List<TaskItem>>(json) ?? [];
 
                 Console.WriteLine("tasks loaded from file:");
-                foreach (var taskItem in tasksItems)
+                foreach (var taskItem in TasksItems)
                 {
                     Console.WriteLine($"task loaded = id: {taskItem.Id}, name: {taskItem.Name}, description: {taskItem.Description}, state = {taskItem.State.ToString()}");
                 }
